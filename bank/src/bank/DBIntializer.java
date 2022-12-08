8129399af88442afbcbfe675809ea3a2e8cb530c
WADEWU <wwaaddeewwuu@gmail.com>
2022-12-09 02:44:33 +0800

    add bank

---
+++ bank/src/bank/DBIntializer.java
@@ -0,0 +1,8 @@
+package bank;
+
+public interface DBIntializer {
+String DRIVER="com.mysql.jdbc.Driver";
+String CON_STRING="jdbc:mysql://localhost:3306/bank";
+String USERNAME="root";
+String PASSWORD="haosql";
+}
