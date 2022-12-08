7309a8f2fad1998b0c92cdbb4bf9212ac2c80776
WADEWU <wwaaddeewwuu@gmail.com>
2022-12-09 01:56:25 +0800

    add shopshop

---
+++ shopshop/src/servlet/LoginServlet.java
@@ -0,0 +1,90 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Cart;
+import dao.UserDAOImpl;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAOImpl userDAO = new UserDAOImpl();
+	private List<Cart> cart = new ArrayList<Cart>();
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		String err = "";
+		if (username.equals("") || password.equals("")) {
+			err += "Phải nhập đầy đ�? thông tin!";
+		} else {
+			if (userDAO.login(username, password) == false) {
+				err += "Tên đăng nhập hoặc mật khẩu không chính xác!";
+			}
+		}
+
+		if (err.length() > 0) {
+			request.setAttribute("err", err);
+		}
+
+		String url = "/login.jsp";
+		try {
+			if (err.length() == 0) {
+				HttpSession session = request.getSession();
+				session.setAttribute("username", username);
+				session.setAttribute("cart", cart);
+				userDAO.login(username, password);
+				Cookie loginCookie = new Cookie("username",username);
+	            //setting cookie to expiry in 30 mins
+	            loginCookie.setMaxAge(30*60);
+	            response.addCookie(loginCookie);
+	            response.sendRedirect("index.jsp");
+				url = "/index.jsp";
+			} else {
+				url = "/login.jsp";
+				RequestDispatcher rd = getServletContext()
+						.getRequestDispatcher(url);
+				rd.forward(request, response);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("/login.jsp");
+		}
+	}
+
+}
